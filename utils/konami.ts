6c6b6a88c38540362a52cf444bbe05035368ab1f
Julien Tanay <julien.tanay@gmail.com>
2021-07-29 09:26:01 +0200

    :label: Fix types

--- utils/konami.ts
+++ utils/konami.ts
@@ -1,4 +1,4 @@
-export const onKonami = function (f, a?) {
+export const onKonami = (f: any, a?: any) => {
   document.onkeyup = function (e) {
     /113302022928$/.test((a += [(e || (self.event as any)).keyCode - 37])) &&
       f();
