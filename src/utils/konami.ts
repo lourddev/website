b4eb2ec9d3a6bcfb43ff60428e8686319b57fc61
Julien Tanay <julien.tanay@gmail.com>
2025-05-02 22:46:12 +0200

    fix: disable eslint warnings

--- src/utils/konami.ts
+++ src/utils/konami.ts
@@ -1,6 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const onKonami = (f: any, a?: any) => {
   document.onkeyup = function (e) {
-    /113302022928$/.test((a += [(e || (self.event as any)).keyCode - 37])) &&
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    /113302022928$/.test((a += [(e || (self.event as Event)).keyCode - 37])) &&
       f();
   };
 };
