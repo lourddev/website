30ce7045ac4b96bd54a393acfb77687fb526c8ee
Julien Tanay <julien.tanay@gmail.com>
2025-05-03 08:18:21 +0200

    feat: small call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   });
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
+    <div className="flex flex-col items-center justify-center h-screen text-center gap-4">
       <Image
         src="/svg/logo.svg"
         alt="Lourd logo"
@@ -24,28 +24,49 @@ export default function Home() {
 
       <div className="text-8xl font-bold ">lourd</div>
       <div>Twitch Extensions & Bots • Streaming & Interactivity tools</div>
+      <div>
+        <a href="mailto:hello@lourd.dev" className="hover:underline">work with us</a>
+      </div>
       <div className="flex gap-2 mt-4">
-        <Image
-          src="/svg/discord.svg"
-          alt="Discord Logo"
-          width={30}
-          height={30}
-          className="inline"
-        />
-        <Image
-          src="/svg/globe.svg"
-          alt="Status logo"
-          width={30}
-          height={30}
-          className="inline"
-        />
-        <Image
-          src="/svg/twitter.svg"
-          alt="Twitter logo"
-          width={30}
-          height={30}
-          className="inline"
-        />
+        <a
+          href="https://invite.gg/lourd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/svg/discord.svg"
+            alt="Discord Logo"
+            width={30}
+            height={30}
+            className="inline"
+          />
+        </a>
+        <a
+          href="https://status.lourd.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/svg/globe.svg"
+            alt="Status logo"
+            width={30}
+            height={30}
+            className="inline"
+          />
+        </a>
+        <a
+          href="https://twitter.com/lourddev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/svg/twitter.svg"
+            alt="Twitter logo"
+            width={30}
+            height={30}
+            className="inline"
+          />
+        </a>
       </div>
     </div>
   );
